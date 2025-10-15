d39f4e1f0a8f421bfcafaf5bee04b607695dbde9
stefanocasagrande-hash <stefano.casagrande@dmodolomiti.it>
2025-10-15 21:53:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.title("📊 Dashboard Turismo Veneto")
 # 🔐 ACCESSO CON PASSWORD
 # ======================
 password = st.text_input("Inserisci password", type="password")
-if password != "veneto2025":
+if password != "segreta123":
     if password:
         st.error("❌ Password errata. Riprova.")
     st.stop()
