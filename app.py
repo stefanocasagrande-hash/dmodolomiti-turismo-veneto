4854bd6020f6e50bc7c3252ff4189b956b29b87d
stefanocasagrande-hash <stefano.casagrande@dmodolomiti.it>
2025-10-22 20:06:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ st.success("✅ Accesso consentito")
 # ======================
 st.sidebar.header("⚙️ Filtri principali – Dati Comunali")
 
-data = load_dati_comunali("dolomiti-turismo-veneto/dati-mensili-per-comune")
+data = load_data("dolomiti-turismo-veneto/dati-mensili-per-comune")
 provincia = load_provincia_belluno("dolomiti-turismo-veneto/dati-provincia-annuali")
 stl_dolomiti, stl_belluno = load_stl_data("dolomiti-turismo-veneto/stl-presenze-arrivi")
 
